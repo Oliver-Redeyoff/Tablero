f3e5a8cb638e151c7eb7e7c495f8e32c5cfee0c5
Oliver-Redeyoff <oliver.redeyoff@gmail.com>
2021-03-26 15:06:13 +0000

    Added calendar widget

---
+++ Raspy/Widgets/calendar_widget.py
@@ -0,0 +1,54 @@
+from PIL import Image,ImageDraw,ImageFont
+from datetime import datetime
+from calendar import monthrange
+import textwrap
+
+class widget:
+
+    def __init__(self, config, tileSize):
+        self.config = config
+        self.tileSize = tileSize
+        print('todo widget initialised')
+
+    def render(self):
+        print('rendering note')
+        bg_color = 255 if self.config['config']['colorMode']=='light' else 0
+        text_color = 0 if self.config['config']['colorMode']=='light' else 255
+
+        widget_width = self.tileSize*self.config['width'] - 10
+        widget_height = self.tileSize*self.config['height'] - 10
+
+        widget_img = Image.new(mode='1', size=(widget_width, widget_height), color=bg_color)
+        widget_draw = ImageDraw.Draw(widget_img)
+        title_font = ImageFont.load_default()
+        content_font = ImageFont.truetype('OpenSans.ttf', 14)
+        
+        widget_draw.rectangle(xy=[(0, 0), (widget_width, widget_height)], outline=0, width= 5)
+
+        widget_draw.text(xy=(10, 10), text='calendar', font=title_font, fill=text_color)
+
+        today = datetime.now()
+        monthRange = monthrange(today.year, today.month)
+        print(today.day)
+        dateYearSize = content_font.getsize(today.strftime("%B %Y"))
+        widget_draw.text(xy=(widget_width/2 - dateYearSize[0]/2, 10), text=today.strftime("%B %Y"), font=content_font, fill=text_color)
+
+        intial_x = round(widget_width*0.1)
+        x = intial_x
+        y = 40
+        dateCubeWidth = round((widget_width - x*2)/7)
+        dateCubeHeight = round((widget_height-y-20)/4)
+        for date in range(monthRange[0], monthRange[1]+1):
+            dateSize = content_font.getsize(str(date))
+            widget_draw.text(xy=(x - dateSize[0]/2, y), text=str(date), font=content_font, fill=text_color)
+
+            if today.day == date:
+                widget_draw.rectangle(xy=[(x-dateCubeWidth/2, y-2), (x+dateCubeWidth/2, y+dateCubeHeight)], outline=text_color, width= 2)
+
+            x += dateCubeWidth
+            if(x >= widget_width):
+                x = intial_x
+                y += dateCubeHeight
+            
+
+        return widget_img
--- Raspy/widgetLoader.py
+++ Raspy/widgetLoader.py
@@ -2,10 +2,12 @@ import Widgets.note_widget as note_widget
 import Widgets.weather_widget as weather_widget
 import Widgets.todo_widget as todo_widget
 import Widgets.picture_widget as picture_widget
+import Widgets.calendar_widget as calendar_widget
 
 def getWidgeRef(id, widgetData, tileSize):
     if id == 0: return note_widget.widget(widgetData, tileSize)
     if id == 1: return weather_widget.widget(widgetData, tileSize)
     if id == 2: return todo_widget.widget(widgetData, tileSize)
     if id == 3: return picture_widget.widget(widgetData, tileSize)
+    if id == 4: return calendar_widget.widget(widgetData, tileSize)
     else : pass
