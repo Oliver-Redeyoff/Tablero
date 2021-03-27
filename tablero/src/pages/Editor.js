6ab603220069528a31c27a6e9da48d2f74e3d74a
Will Cruse <willcruse123@protonmail.com>
2021-03-27 09:46:54 +0000

    feat(website): Add unified data model

--- tablero/src/pages/Editor.js
+++ tablero/src/pages/Editor.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import { 
     Container, 
     Row, 
@@ -6,55 +6,115 @@ import {
     Tabs,
     Tab,
     CardColumns,
+    CardDeck,
     Card,
     Button,
     Form,
 } from "react-bootstrap";
-import { DndProvider } from "react-dnd"
-import HTML5Backend from "react-dnd-html5-backend"
+import { DndProvider, useDrag, useDrop } from "react-dnd"
+import {HTML5Backend} from "react-dnd-html5-backend"
+import {cloneDeep} from "lodash";
 import sushi from "../images/sushi-food.svg"
 
-const type = "Widget"
+const WidgetDragTypes = {
+    WIDGET: 'widget'
+}
+
+const API_URL = 'https://europe-west2-la-hacks-308508.cloudfunctions.net' 
+
+function createGridWidgetFromWidget(widget, location) {
+    const copiedWidget = cloneDeep(widget);
+    copiedWidget['x'] = location[0]
+    copiedWidget['y'] = location[1]
+    return copiedWidget
+}
 
 function Editor() {
-    const widgets = [
-        {
-            "title": "Notes - small",
-            "description": "Display a note to yourself",
-            "size": [1, 1]
-        },
-        {
-            "title": "Notes - medium",
-            "description": "Display a note to yourself",
-            "size": [2, 1]
-        },
-        {
-            "title": "Notes - large",
-            "description": "Display a note to yourself",
-            "size": [3, 1]
-        },
-        {
-            "title": "Weather - small",
-            "description": "Show the weather",
-            "size": [1, 1]
-        },
-        {
-            "title": "Weather - medium",
-            "description": "Show the weather",
-            "size": [2, 2]
-        },
-        {
-            "title": "Weather - large",
-            "description": "Show the weather",
-            "size": [3, 2]
-        },
-    ]
+
+    const [gridWidgets, changeGridWidgets] = useState([])
+    const [widgets, updateWidgets] = useState([])
+    const [config, setConfig] = useState({})
+    
+    // Calls get-config endpoint
+    useEffect(() => {
+        fetch(API_URL + '/get-config', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'user_id': 'test-new-data',
+                'secret': 'password'
+            })
+        }).then((response) => 
+            response.json()
+        ).then((json) => {
+            if (json.config != null) {
+                setConfig(json.config)
+            }
+            if (json.grid != null) {
+                changeGridWidgets(json.grid.map((item) => {
+                    const parent = widgets[item.id]
+                    if (parent != null) {
+                        const fullWidget = {...item, ...cloneDeep(parent)}
+                        return fullWidget
+                    }
+                    return null
+                    
+                }).filter(ele => ele != null))
+            }
+        }).catch(e => {console.warn(e)})
+    }, [widgets])
+
+    // Calls get-widgets endpoint
+    useEffect(() => {
+        fetch(API_URL + '/get-widgets').then((response) => 
+                response.json()
+            ).then((json) => {
+
+                if (json.widgets != null) {
+                    const widgetConfigs = []
+
+                    for (const widgetConfig of json.widgets) {
+
+                        Object.keys(widgetConfig).forEach((key) => {
+                            const widgetSizes = [];
+                            const baseObject = cloneDeep(widgetConfig[key])
+                            baseObject.id = key;
+
+                            for (let widgetSize of ['sm', 'md', 'lg']) {
+                                if (baseObject[widgetSize] != null) {
+                                    const sizedObject = cloneDeep(baseObject)
+                                    sizedObject.size = baseObject[widgetSize]
+                                    sizedObject.title = baseObject.title + " - " + widgetSize
+                                    sizedObject.id = key + "-" + widgetSize
+                                    widgetSizes.push(sizedObject)
+                                }
+                            }
+
+                            if (widgetSizes.length > 0) {
+                                widgetConfigs.push(...widgetSizes)
+                            } else {
+                                widgetConfigs.push(baseObject)
+                            }
+
+                        })
+ 
+                    }
+                    updateWidgets(widgetConfigs)
+                }
+            }).catch(err => console.warn(err))
+    }, [])
+
     return (
         <DndProvider backend={HTML5Backend}>
             <Container style={{paddingTop: "1rem"}}>
                 <Row>
                     <Col sm="12" lg="6" >
-                        <Viewer />
+                        <Grid 
+                            gridWidgets={gridWidgets} 
+                            changeGridWidgets={changeGridWidgets}
+                        />
                     </Col>
                     <Col sm="12" lg="6" >
                         <ControlPanel widgets={widgets}/>
@@ -65,10 +125,75 @@ function Editor() {
     )
 }
 
-function Viewer() {
 
+function Grid({gridWidgets, changeGridWidgets}) {
+
+    const width = 3
+    const height = 5
+
+    const gridTiles = [];
+    for(let i=0 ; i<width*height ; i++) {
+        const position = [i%width, Math.floor(i/width)]
+        gridTiles.push(<GridTile position={position} changeGridWidgets={changeGridWidgets}/>)
+    }
+    
     return (
-        <h1>Viewer</h1>
+        <div style={{
+            position: "absolute",
+            top: "0px",
+            left: "0px",
+            width: "100%",
+            height: "100%",
+            border: "1px solid red"
+        }}>
+            {gridTiles.map((gridTile) => gridTile)}
+            {gridWidgets.map((gridWidget) => <GridWidget widget={gridWidget} />)}
+        </div>
+    )
+}
+
+function GridTile({position, changeGridWidgets}) {
+    // Droppable target in the Grid
+    const onDrop = (item) => {
+        changeGridWidgets((current) => [...current, createGridWidgetFromWidget(item, position)])
+    }
+
+    const [{}, drop] = useDrop(() => ({
+        accept: WidgetDragTypes.WIDGET,
+        drop: (item) => {onDrop(item)},
+    }))
+    
+    return (
+        <div style={{
+            position: "absolute",
+            top: position[1] * 100/5 + "%",
+            left: position[0] * 100/3 + "%",
+            height: 100/5 + "%",
+            width: 100/3 + "%",
+            padding: "10px"
+        }}
+        ref={drop}>
+        <div style={{backgroundColor: "rgba(0, 0, 0, 0.2)", height: "100%", width: "100%"}}></div></div>
+    )
+}
+
+function GridWidget({widget}) {
+    // Widget overlayed on top of GridTile
+    const size = widget.size
+    
+    return (
+        <div style={{
+            position: "absolute",
+            width: size.width*100/3 + "%",
+            height: size.height*100/5 + "%",
+            left: widget.x*100/3 + "%",
+            top: widget.y*100/5 + "%",
+            padding: "10px"
+        }}>
+            <div style={{backgroundColor: "rgba(0, 0, 0)", height: "100%", width: "100%"}}>
+                <p style={{color: "white"}}>{widget.title}</p>
+            </div>
+        </div>
     )
 }
 
@@ -90,16 +215,21 @@ function Widgets({widgets}) {
     
     return (
         <CardColumns style={{paddingTop: "1rem"}}>
-            {widgets.map(e => <Widget widgetInfo={e} />)}
+            {Object.values(widgets).map(e => <Widget widgetInfo={e} />)}
         </CardColumns>
     )
 }
 
 function Widget({widgetInfo}) {
 
+    const [_, drag] = useDrag(() => ({
+        type: WidgetDragTypes.WIDGET,
+        item: widgetInfo,
+      }))
+    
     return (
-        <Card>
-            <Card.Img variant="top" src={sushi} />
+        <Card ref={drag}>
+            <Card.Img variant="top" src={sushi} style={{pointerEvents: "none"}} />
             <Card.Body>
                 <Card.Title>{widgetInfo?.title}</Card.Title>
                 {widgetInfo?.description != null ? <Card.Text>{widgetInfo.description}</Card.Text> : <></>}
