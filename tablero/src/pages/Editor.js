d8f3cfda3f2a03755ecbbe13f09310c3bb4bbab8
Will Cruse <willcruse123@protonmail.com>
2021-03-28 09:56:50 +0100

    feat(website): Add alert to tell user when they failed to login

--- tablero/src/pages/Editor.js
+++ tablero/src/pages/Editor.js
@@ -11,6 +11,7 @@ import {
     Button,
     Form,
     Modal,
+    Alert,
 } from "react-bootstrap";
 import { DndProvider, useDrag, useDrop } from "react-dnd"
 import {HTML5Backend} from "react-dnd-html5-backend"
@@ -40,6 +41,7 @@ function Editor() {
 
 
     const [userLoggedIn, changeUserLoggedIn] = useState(false)
+    const [failedLogin, setFailedLogin] = useState(false)
     const [deviceName, updateDeviceName] = useState("")
     const [secret, updateSecret] = useState("")
     
@@ -130,8 +132,14 @@ function Editor() {
             })}).then((resp) => resp.json()).then((json) => {
                 if (json.success === true) {
                     changeUserLoggedIn(true);
-
+                } else {
+                    setFailedLogin(true)
+                    updateDeviceName("")
+                    updateSecret("")
                 }
+            }).catch((err) => {
+                setFailedLogin(true)
+                console.warn(err)
             })
         }
     
@@ -188,6 +196,7 @@ function Editor() {
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
+                    {failedLogin ? <Alert variant="danger">Login failed</Alert> : <></>}
                     <Button variant="secondary" onClick={handleLogin}>Sign in</Button>
                 </Modal.Footer>
             </Modal>
@@ -315,8 +324,6 @@ function Widgets({widgets}) {
 }
 
 function Widget({widgetInfo}) {
-
-    console.log(widgetInfo)
 
     const [_, drag] = useDrag(() => ({
         type: WidgetDragTypes.WIDGET,
